1d94cd2903f75bb9dc9ab78f09c3700c4483d518
zhangyongwang <349368992@qq.com>
2018-11-11 20:34:47 +0800

    element-ui的使用

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -1,7 +1,31 @@
 export default {
-    data() {
-      return {
-        msg: "Welcome to Your Vue.js App"
-      };
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    onClick1() {
+      this.$alert('FCH前途无量', '暖通吧', {
+        confirmButtonText: '确定',
+        callback: action => {
+          this.$message({
+            type: 'info',
+            message: 'fch'
+          });
+        }
+      });
+    },
+    onClick2() {
+      this.$alert('牛的一批', '德国索热', {
+        confirmButtonText: '确定',
+        callback: action => {
+          this.$message({
+            type: 'info',
+            message: 'sorel'
+          });
+        }
+      });
     }
-  };
+  }
+};
--- src/pages/Index/template.vue
+++ src/pages/Index/template.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="index">
-  <p>博客首页</p>
+  <p>首页</p>
+  <div>
+    <el-button type="primary" @click="onClick1">FCH</el-button>
+    <el-button type="success" @click="onClick2">SOREL</el-button>
+  </div>
+
   </div>
 </template>
 <script src="./template.js"></script>
