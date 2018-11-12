1e1f5e439c902a9246fbe9dc274124ad10ab36b8
zhangyongwang <349368992@qq.com>
2018-11-12 20:29:02 +0800

    添加 header.login header.no-login 的样式

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header>
+    <header :class="{login:isLogin,'no-login':!isLogin}">
       <template v-if="!isLogin">
           <h1>FCH联盟成员博客系统</h1>
           <p>项目案例汇总</p>
@@ -10,7 +10,9 @@
       </template>
 
       <template v-if="isLogin">
-          <h1>Let's share</h1>
+          <h1>FCH</h1>
+          <i class="edit el-icon-edit"></i>
+          <img class="avatar" src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" alt="">
       </template>
       
     </header>
@@ -29,7 +31,7 @@ export default {
 <style lang="less">
 @import "../assets/base.less";
 
-header {
+header.no-login {
   padding: 0 12% 30px 12px;
   background: @bgColor;
   display: grid;
@@ -56,6 +58,66 @@ header {
 
   button {
     margin: 20px 5px 0;
+  }
+}
+
+header.login {
+  display: flex;
+  align-items: center;
+  background: @bgColor;
+  h1 {
+    margin: 0;
+    padding: 0;
+    font-size: 40px;
+    text-transform: uppercase;
+    flex: 1;
+    color: #fff;
+    a {
+      color: #fff;
+    }
+  }
+
+  .edit {
+    color: #fff;
+    font-size: 30px;
+  }
+
+  .avatar {
+    width: 40px;
+    height: 40px;
+    border: 1px solid #fff;
+    border-radius: 50%;
+    margin-left: 15px;
+  }
+  .user {
+    position: relative;
+
+    ul {
+      display: none;
+      position: absolute;
+      right: 0;
+      list-style: none;
+      border: 1px solid #eaeaea;
+      margin: 0;
+      padding: 0;
+      background-color: #fff;
+
+      a {
+        text-decoration: none;
+        color: #333;
+        font-size: 12px;
+        display: block;
+        padding: 5px 10px;
+
+        &:hover {
+          background-color: #eaeaea;
+        }
+      }
+    }
+
+    &:hover ul {
+      display: block;
+    }
   }
 }
 </style>
