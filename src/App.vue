1818cce87b7ce0461744645259c39a869827d552
zhangyongwang <349368992@qq.com>
2018-11-12 15:39:49 +0800

    引入 header footer 组件

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,19 @@
 </template>
 
 <script>
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    Header,
+    Footer
+  }
 };
 </script>
 
+
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer>
+      <p>nuantongba.com</p>
+  </footer>
+</template>
