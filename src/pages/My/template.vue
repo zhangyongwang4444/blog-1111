1f06180558a2efa19db1660199dbdca412511a18
zhangyongwang <349368992@qq.com>
2018-11-13 09:43:22 +0800

    my 页面的 结构 样式

--- src/pages/My/template.vue
+++ src/pages/My/template.vue
@@ -1,7 +1,27 @@
 <template>
-  <div id="my">
-  我的页面
+  <div id="user">
+    <section class="user-info">
+      <img src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" class="avatar">
+      <h3>张永旺</h3>
+    </section>
+    <section>
+      <a class="item">
+        <div class="date">
+          <span class="day">13</span>
+          <span class="month">11月</span>
+          <span class="year">2018年</span>
+        </div>
+        <h3>宁波FCH体验间施工记录</h3>
+        <p>哈哈哈哈哈</p>
+        <div class="actions">
+          <a>编辑</a>
+          <a href="#" >删除</a>
+        </div>
+      </a>
+    </section>
   </div>
 </template>
+
 <script src="./template.js"></script>
-<style src="./template.css"></style>
+
+<style src="../My/template.less" lang="less"></style>
