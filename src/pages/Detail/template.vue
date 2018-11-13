9a10087c3e1c1f0a2314f2043d8bc6b56a74e1e1
zhangyongwang <349368992@qq.com>
2018-11-13 09:22:02 +0800

    详情页 detail 的 结构和样式

--- src/pages/Detail/template.vue
+++ src/pages/Detail/template.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="detail">
-  博客详情页面
+    <section class="user-info">
+      <img src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" class="avatar">
+      <h3>FCH标题</h3>
+      <p>张永旺<span>发布于5天前</span></p>
+    </section>
+    <section class="article">
+      <p>哦我我我我iv那都是v你的手</p>
+    </section>
   </div>
 </template>
+
 <script src="./template.js"></script>
-<style src="./template.css"></style>
+
+<style src="./template.less" lang="less"></style>
