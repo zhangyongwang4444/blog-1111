14413bb05946330f8af4bebc49ed83008a527b8e
zhangyongwang <349368992@qq.com>
2018-11-14 14:38:54 +0800

    完善header的 router-link

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,8 +4,8 @@
           <h1>FCH项目系统</h1>
           <p>项目案例汇总</p>
           <div class="btns">
-            <el-button>立即登录</el-button>
-            <el-button>注册账号</el-button>
+            <router-link to='/login'><el-button>立即登录</el-button></router-link>
+            <router-link to='/register'><el-button>注册账号</el-button></router-link>
           </div>
       </template>
 
