ededf0700228b4d63caf5e478796c70ab9780055
zhangyongwang <349368992@qq.com>
2018-11-14 08:18:02 +0800

    auth的 VUEX 书写

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,14 +1,30 @@
+import auth from '@/api/auth'
 const state = {
-
+    user: null,
+    isLogin: false
 }
 const getters = {
 
 }
 const mutations = {
-
+    setUser(state, payload) {
+        state.user = payload.user
+    },
+    setLogin(state, payload) {
+        state.isLogin = payload.isLogin
+    }
 }
 const actions = {
+    login({ commit }, { username, password }) {
+        return auth.login({ username, password })
+            .then(res => {
+                commit('setUser', { user: res.data })
+                commit('setLogin', { isLogin: true })
+            })
+    },
+    register() {
 
+    }
 }
 export default {
     state,
